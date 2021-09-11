e314dedfa269c5142fe354c12865712e62948487
romulo <romulot.almeida@gmail.com>
2021-09-11 19:36:24 -0300

    montando a estrutura base

---
+++ src/knexfile.js
@@ -0,0 +1,22 @@
+// Update with your config settings.
+
+module.exports = {
+
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      database: 'graphql',
+      user:     'root',
+      password: 'root'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
